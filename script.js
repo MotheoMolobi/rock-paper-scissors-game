817fa10b39fd89c56ac4466477efda2beb4f4890
MotheoMolobi <molobimotheo@gmail.com>
2024-10-18 09:21:10 +0200

    human function completed

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//Scores
+let humanScore = 0;
+let computerScore = 0; 
 
 function getComputerChoice() {
     computerNum = parseInt(Math.random() *10);
@@ -14,4 +17,15 @@ function getComputerChoice() {
 }
 //check for computer choice
 //console.log(getComputerChoice());
+
+function getHumanChoice() {
+    humanChoice = parseInt(prompt("Enter your choice\n rock = 1\n paper = 2\n scissors = 3" ));
+    return humanChoice;
+}
+//Calling the human choice function
+getHumanChoice();
+
+function playRound(humanChoice, computerChoice) {
+    
+}
 
